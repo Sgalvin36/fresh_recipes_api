6fc912dde04164c3d1384eca81dce41bb65fdf9b
Shane Galvin <Sgalvin36@gmail.com>
2024-10-30 15:35:59 -0600

    All migrations to get to initial schema

--- db/migrate/20241030210842_add_recipe_ref_to_recipe_instructions.rb
+++ db/migrate/20241030210842_add_recipe_ref_to_recipe_instructions.rb
@@ -1,5 +1,5 @@
 class AddRecipeRefToRecipeInstructions < ActiveRecord::Migration[7.1]
   def change
-    add_reference :recipe_instructions, :recipe_id, null: false, foreign_key: true
+    add_reference :recipe_instructions, :recipe, null: false, foreign_key: true
   end
 end
--- db/migrate/20241030211058_add_measurement_ref_to_recipe_ingredients.rb
+++ db/migrate/20241030211058_add_measurement_ref_to_recipe_ingredients.rb
@@ -1,7 +1,7 @@
 class AddMeasurementRefToRecipeIngredients < ActiveRecord::Migration[7.1]
   def change
-    add_reference :recipe_ingredients, :measurement_id, null: false, foreign_key: true
-    add_reference :recipe_ingredients, :recipe_id, null: false, foreign_key: true
-    add_reference :recipe_ingredients, :ingredient_id, null: false, foreign_key: true
+    add_reference :recipe_ingredients, :measurement, null: false, foreign_key: true
+    add_reference :recipe_ingredients, :recipe, null: false, foreign_key: true
+    add_reference :recipe_ingredients, :ingredient, null: false, foreign_key: true
   end
 end
