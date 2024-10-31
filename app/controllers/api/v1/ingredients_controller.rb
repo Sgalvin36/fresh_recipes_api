c2522935eda0d42db0eff952d1cf4bff87dc580c
Shane Galvin <Sgalvin36@gmail.com>
2024-10-31 15:56:54 -0600

    Created recipe serializer

--- app/controllers/api/v1/ingredients_controller.rb
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::IngredientsController < ApplicationController
         else
             ingredients = Ingredient.all
         end
-        render json: IngredientSerializer.format_recipes(recipes)
+        render json: IngredientSerializer.new(ingredients)
 
     end
 
