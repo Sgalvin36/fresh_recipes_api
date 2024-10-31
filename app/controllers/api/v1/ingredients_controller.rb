a79b80a5c0fe8979fcd6eed12c66f9a9da35854a
Shane Galvin <Sgalvin36@gmail.com>
2024-10-31 15:28:15 -0600

    Added tests for ingredients controller

---
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::IngredientsController < ApplicationController
+    
+    def index
+        if params[:for_ingredient]
+            ingredients = Ingredient.find(params[:for_ingredient])
+        else
+            ingredients = Ingredient.all
+        end
+        render json: RecipeSerializer.format_recipes(recipes)
+
+    end
+
+end
---
+++ spec/requests/api/v1/ingredient_request_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "Ingredient controller", type: :request do
+    describe "GET all ingredients Endpoint" do
+        it "can make the request successfully" do
+            headers = { 
+                "CONTENT_TYPE" => "application/json",
+            }
+
+            get api_v1_ingredients_path
+            expect(response).to be_successful
+        end
+
+        xit "can return the data as requested" do
+            ingredient = Ingredient.create!(name:"American Cheese", national_price:4.32, taxable:true, snap:true)
+            ingredient2 = Ingredient.create!(name:"Bread", national_price:2.43, taxable:false, snap:true)
+            
+            headers = { 
+                "CONTENT_TYPE" => "application/json",
+            }
+
+            get api_v1_recipes_path
+            expect(response).to be_successful
+        
+            json = JSON.parse(response.body, symbolize_names:true)
+            json[:data].each do |ingredient|
+                expect(ingredient).to have_key(:id)
+                expect(ingredient).to have_key(:name)
+            end
+        end
+    end
+end
