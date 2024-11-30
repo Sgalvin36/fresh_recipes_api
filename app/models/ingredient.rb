4a8fd480d72c45e3cbcaf32f66240db0b5749050
Shane Galvin <Sgalvin36@gmail.com>
2024-11-30 14:24:56 -0700

    Added validation for kroger_id

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -6,7 +6,7 @@ class Ingredient < ApplicationRecord
   validates :national_price, numericality: { greater_than: 0, only_float: true }
   validates :taxable, inclusion: { in: [true, false] }
   validates :snap, inclusion: { in: [true, false] }
-  # validates :kroger_id, presence: true
+  validates :kroger_id, presence: true
   after_save :update_associated_recipes_total_price, if: :saved_change_to_national_price?
 
   def self.filter_ingredients(search_params)
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -9,14 +9,13 @@ RSpec.describe Ingredient, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_numericality_of(:national_price).is_greater_than(0) }
-    # UNCOMMENT ONCE SEEDED INGREDIENT DATA CONTAINS LIVE kroger_id ATTRIBUTES
-    # it { should validate_presence_of(:kroger_id) }
+    it { should validate_presence_of(:kroger_id) }
   end
 
   context "validating boolean values" do
     it 'validates taxable to be either true or false' do
-      ingredient_true = Ingredient.new(name: "test_taxable", national_price: 10, taxable: true, snap: true)
-      ingredient_false = Ingredient.new(name: "test_taxable", national_price: 10, taxable: false, snap: true)
+      ingredient_true = Ingredient.new(name: "test_taxable", national_price: 10, taxable: true, snap: true, kroger_id: 32)
+      ingredient_false = Ingredient.new(name: "test_taxable", national_price: 10, taxable: false, snap: true, kroger_id: 32)
       
       ingredient_nil = Ingredient.new(name: "test_taxable", national_price: 10, taxable: nil, snap: true)
       
@@ -27,8 +26,8 @@ RSpec.describe Ingredient, type: :model do
     end
 
     it 'validates snap to be either true or false' do
-      ingredient_true = Ingredient.new(name: "test_snap", national_price: 10, taxable: true, snap: true)
-      ingredient_false = Ingredient.new(name: "test_snap", national_price: 10, taxable: true, snap: false)
+      ingredient_true = Ingredient.new(name: "test_snap", national_price: 10, taxable: true, snap: true, kroger_id: 32)
+      ingredient_false = Ingredient.new(name: "test_snap", national_price: 10, taxable: true, snap: false, kroger_id: 32)
       
       ingredient_nil = Ingredient.new(name: "test_snap", national_price: 10, taxable: true, snap: nil)
       
@@ -41,14 +40,14 @@ RSpec.describe Ingredient, type: :model do
 
   describe "model methods" do
     it "returns no more than 5 ingredients that match a partial query" do
-      ingredient = Ingredient.create!(name:"American Cheese", national_price:4.32, taxable:true, snap:true)
-      ingredient2 = Ingredient.create!(name:"Bread", national_price:2.43, taxable:false, snap:true)
-      ingredient3 = Ingredient.create!(name:"Blue Cheese", national_price:6.32, taxable:true, snap:true)
-      ingredient4 = Ingredient.create!(name:"Hot Dogs", national_price:4.73, taxable:false, snap:true)
-      ingredient5 = Ingredient.create!(name:"French Cheese", national_price:4.32, taxable:true, snap:true)
-      ingredient6 = Ingredient.create!(name:"Red Cheese", national_price:2.43, taxable:false, snap:true)
-      ingredient7 = Ingredient.create!(name:"Green Cheese", national_price:6.32, taxable:true, snap:true)
-      ingredient8 = Ingredient.create!(name:"Cheese", national_price:4.73, taxable:false, snap:true)
+      ingredient = Ingredient.create!(name:"American Cheese", national_price:4.32, taxable:true, snap:true, kroger_id: 32)
+      ingredient2 = Ingredient.create!(name:"Bread", national_price:2.43, taxable:false, snap:true, kroger_id: 32)
+      ingredient3 = Ingredient.create!(name:"Blue Cheese", national_price:6.32, taxable:true, snap:true, kroger_id: 32)
+      ingredient4 = Ingredient.create!(name:"Hot Dogs", national_price:4.73, taxable:false, snap:true, kroger_id: 32)
+      ingredient5 = Ingredient.create!(name:"French Cheese", national_price:4.32, taxable:true, snap:true, kroger_id: 32)
+      ingredient6 = Ingredient.create!(name:"Red Cheese", national_price:2.43, taxable:false, snap:true, kroger_id: 32)
+      ingredient7 = Ingredient.create!(name:"Green Cheese", national_price:6.32, taxable:true, snap:true, kroger_id: 32)
+      ingredient8 = Ingredient.create!(name:"Cheese", national_price:4.73, taxable:false, snap:true, kroger_id: 32)
 
       result = Ingredient.filter_ingredients("cHeE")
       expected_result = [ingredient, ingredient3, ingredient5, ingredient7, ingredient8].sort_by(&:name)
@@ -61,8 +60,8 @@ RSpec.describe Ingredient, type: :model do
       recipe_1 = Recipe.create!(name: "test_1", total_price: 0.00, image: "test_image_1.png", serving_size: 1)
       recipe_2 = Recipe.create!(name: "test_2", total_price: 0.00, image: "test_image_2.png", serving_size: 1)
       
-      shared_ingredient = Ingredient.create!(name: "test_1", national_price: 1.00, taxable: false, snap: true)
-      unique_ingredient = Ingredient.create!(name: "test_2", national_price: 2.00, taxable: false, snap: true)
+      shared_ingredient = Ingredient.create!(name: "test_1", national_price: 1.00, taxable: false, snap: true, kroger_id: 32)
+      unique_ingredient = Ingredient.create!(name: "test_2", national_price: 2.00, taxable: false, snap: true, kroger_id: 32)
 
       measurement = Measurement.create!(unit: "each")
       
